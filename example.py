617880259603a6da451f68a22feed69911662832
Sean-Jiun Wang <churchguitarist@gmail.com>
2017-12-04 02:55:53 -0600

    Better example

--- example.py
+++ example.py
@@ -1,4 +1,7 @@
+import os
+
 from cmslpc_postproc import PostProcessJobs
+
 
 jobs = PostProcessJobs()
 
@@ -14,6 +17,11 @@ jobs.submit(
     tag='TT_powheg',
     src='root://cmseos.fnal.gov///store/group/lpchbb/NanoTestProd010/TT_TuneCUETP8M2T4_13TeV-powheg-pythia8/RunIISummer17MiniAOD-92X-NanoCrabProd010/171128_160136/',
     dst='root://cmseos.fnal.gov///store/user/sjwang/NanoTestPostProc/TT_TuneCUETP8M2T4_13TeV-powheg-pythia8/',
+    commands={
+        'x509userproxy': os.environ['X509_USER_PROXY'],
+        # The maximum runtime in seconds.
+        '+MaxRuntime': 3600,
+    },
     no_submit=True,
 )
 
