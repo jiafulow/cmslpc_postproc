444c168468a473a24ce3a342a3915b737a615138
Jia Fu Low <jia.fu.low@cern.ch>
2018-02-11 04:14:17 -0500

    update

--- example.py
+++ example.py
@@ -7,13 +7,13 @@ jobs = PostProcessJobs()
 
 
 input_dir = '/home/jlow/L1MuonTrigger/P2_CMSSW_9_2_3_patch1/src/L1TMuonSimulations/Analyzers/test2/'
-input_files = ['rootpy_trackbuilding3.py', 'histos_tb.8.npz', 'encoder.8.npz', 'model.8.h5', 'model_weights.8.h5', 'bpca.8.npz']
+input_files = ['rootpy_trackbuilding3.py', 'histos_tb.9.npz', 'encoder.9.npz', 'model.9.h5', 'model_weights.9.h5']
 prepend_input_dir = lambda x: os.path.join(input_dir, x)
 jobs.pack(map(prepend_input_dir, input_files))
 
 jobs.submit(
     tag='jftest1',
-    src=range(10),
+    src=range(30),
     dst='',
     analysis='rates',
     no_submit=True,
@@ -24,7 +24,7 @@ jobs.submit(
 
 jobs.submit(
     tag='jftest2',
-    src=range(10),
+    src=range(20),
     dst='',
     analysis='effie',
     no_submit=True,
