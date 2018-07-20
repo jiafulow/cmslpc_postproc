7156fe68614d463ed67572d6e9a4b56c16bebad6
Jia Fu Low <jia.fu.low@cern.ch>
2018-07-20 14:29:38 -0400

    update

--- templates_jftest/worker.sh
+++ templates_jftest/worker.sh
@@ -13,78 +13,75 @@ readonly TARBALL="default.tgz"
 readonly ANALYSIS="$1"
 readonly JOBID="$2"
 
-main() {
-  echo "$(date) - $CONDOR_EXEC - INFO - condor_scratch: $_CONDOR_SCRATCH_DIR"
-  echo "$(date) - $CONDOR_EXEC - INFO - pwd: $PWD"
-  echo "$(date) - $CONDOR_EXEC - INFO - args: $ANALYSIS $JOBID"
 
-  echo "$(date) - $CONDOR_EXEC - INFO - Unpacking files"
-  tar xzf $TARBALL
+echo "$(date) - $CONDOR_EXEC - INFO - condor_scratch: $_CONDOR_SCRATCH_DIR"
+echo "$(date) - $CONDOR_EXEC - INFO - pwd: $PWD"
+echo "$(date) - $CONDOR_EXEC - INFO - args: $ANALYSIS $JOBID"
 
-  echo "$(date) - $CONDOR_EXEC - INFO - Setting up $SUBMIT_CMSSW_VERSION"
+echo "$(date) - $CONDOR_EXEC - INFO - Unpacking files"
+tar xzf $TARBALL
 
-  # Setup the CMS software environment.
-  export SCRAM_ARCH="$SUBMIT_SCRAM_ARCH"
-  source /cvmfs/cms.cern.ch/cmsset_default.sh
+echo "$(date) - $CONDOR_EXEC - INFO - Setting up $SUBMIT_CMSSW_VERSION"
 
-  # Checkout the CMSSW release and set the runtime environment. These
-  # commands are often invoked by their aliases "cmsrel" and "cmsenv".
-  #scram project CMSSW "$SUBMIT_CMSSW_VERSION"
-  cd "$SUBMIT_CMSSW_VERSION/src"
-  eval "$(scramv1 runtime -sh)"
+# Setup the CMS software environment.
+export SCRAM_ARCH="$SUBMIT_SCRAM_ARCH"
+source /cvmfs/cms.cern.ch/cmsset_default.sh
 
-  echo "$(date) - $CONDOR_EXEC - INFO - Setting up virtualenv"
-  source venv/bin/activate
+# Checkout the CMSSW release and set the runtime environment. These
+# commands are often invoked by their aliases "cmsrel" and "cmsenv".
+#scram project CMSSW "$SUBMIT_CMSSW_VERSION"
+cd "$SUBMIT_CMSSW_VERSION/src"
+eval "$(scramv1 runtime -sh)"
 
-  # Change back to the worker node's scratch directory.
-  cd "$_CONDOR_SCRATCH_DIR"
+echo "$(date) - $CONDOR_EXEC - INFO - Setting up virtualenv"
+source venv/bin/activate
 
-  echo "$(date) - $CONDOR_EXEC - INFO - pwd: $PWD"
+# Change back to the worker node's scratch directory.
+cd "$_CONDOR_SCRATCH_DIR"
 
-  echo "$(date) - $CONDOR_EXEC - INFO - ls"
-  ls -l
+echo "$(date) - $CONDOR_EXEC - INFO - pwd: $PWD"
 
-  # Do Science
-  echo "$(date) - $CONDOR_EXEC - INFO - stand back I'm going to try Science!"
+echo "$(date) - $CONDOR_EXEC - INFO - ls"
+ls -l
 
-  python rootpy_trackbuilding5.py $ANALYSIS $JOBID
+# Do Science
+echo "$(date) - $CONDOR_EXEC - INFO - Stand back I'm going to try Science!"
 
-  EXIT_STATUS=$?
-  ERROR_TYPE=""
-  ERROR_MESSAGE="This is an error message."
+python rootpy_trackbuilding5.py $ANALYSIS $JOBID
 
-  echo "$(date) - $CONDOR_EXEC - INFO - clean up etc"
+EXIT_STATUS=$?
+ERROR_TYPE=""
+ERROR_MESSAGE="This is an error message."
 
-  # Rename output files
-  [ -f histos_tba.root ] && mv histos_tba.root histos_tba_$JOBID.root
-  [ -f histos_tba.npz  ] && mv histos_tba.npz  histos_tba_$JOBID.npz
-  [ -f histos_tbb.root ] && mv histos_tbb.root histos_tbb_$JOBID.root
-  [ -f histos_tbb.npz  ] && mv histos_tbb.npz  histos_tbb_$JOBID.npz
-  [ -f histos_tbc.root ] && mv histos_tbc.root histos_tbc_$JOBID.root
-  [ -f histos_tbc.npz  ] && mv histos_tbc.npz  histos_tbc_$JOBID.npz
-  [ -f histos_tbd.root ] && mv histos_tbd.root histos_tbd_$JOBID.root
-  [ -f histos_tbd.npz  ] && mv histos_tbd.npz  histos_tbd_$JOBID.npz
+echo "$(date) - $CONDOR_EXEC - INFO - Postprocessing"
 
-  # Prepare reports
-  if [ $EXIT_STATUS -ne 0 ]; then
-    cat << EOF > FrameworkJobReport.xml
+# Rename output files
+[ -f histos_tba.root ] && mv histos_tba.root histos_tba_$JOBID.root
+[ -f histos_tba.npz  ] && mv histos_tba.npz  histos_tba_$JOBID.npz
+[ -f histos_tbb.root ] && mv histos_tbb.root histos_tbb_$JOBID.root
+[ -f histos_tbb.npz  ] && mv histos_tbb.npz  histos_tbb_$JOBID.npz
+[ -f histos_tbc.root ] && mv histos_tbc.root histos_tbc_$JOBID.root
+[ -f histos_tbc.npz  ] && mv histos_tbc.npz  histos_tbc_$JOBID.npz
+[ -f histos_tbd.root ] && mv histos_tbd.root histos_tbd_$JOBID.root
+[ -f histos_tbd.npz  ] && mv histos_tbd.npz  histos_tbd_$JOBID.npz
+
+# Prepare reports
+if [ $EXIT_STATUS -ne 0 ]; then
+  cat << EOF > FrameworkJobReport.xml
 <FrameworkJobReport>
 <FrameworkError ExitStatus="$EXIT_STATUS" Type="$ERROR_TYPE" >
 $ERROR_MESSAGE
 </FrameworkError>
 </FrameworkJobReport>
 EOF
-  fi
+fi
 
-  # Clean up
-  tar tzf $TARBALL | xargs rm -rf
-  rm $TARBALL
+# Clean up
+tar tzf $TARBALL | xargs rm -rf
+rm $TARBALL
 
-  echo "$(date) - $CONDOR_EXEC - INFO - ls"
-  ls -l
+echo "$(date) - $CONDOR_EXEC - INFO - ls"
+ls -l
 
-  echo "$(date) - $CONDOR_EXEC - INFO - Science complete!"
-  exit $EXIT_STATUS
-}
-main
-
+echo "$(date) - $CONDOR_EXEC - INFO - Science complete!"
+exit $EXIT_STATUS
