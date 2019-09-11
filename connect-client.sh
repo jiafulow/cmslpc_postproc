5987eb89176d92e938d9b11711fc7e4027d089f0
Jia Fu Low <jia.fu.low@cern.ch>
2019-09-11 00:47:41 -0400

    Add connect-client.sh

---
+++ connect-client.sh
@@ -0,0 +1,2 @@
+# https://twiki.cern.ch/twiki/bin/view/CMSPublic/WorkBookCMSConnect#Using_the_Connect_client
+wget -O - https://raw.githubusercontent.com/CMSConnect/cmsconnect-client/master/cmsconnect_client_install.sh | bash 
